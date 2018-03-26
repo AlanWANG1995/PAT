33c9f9fa92e9ffa64ff9410c9b068959f9e50110
AlanWANG1995 <alan1995wang@outlook.com>
2018-03-26 19:29:35 +0800

    Create 1001.cc
    create 1001 test

---
+++ Basic/1001.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+  //从控制台读取输入值
+  int input;
+  scanf("%d", &input);
+  
+  //初始化步数
+  int steps = 0;
+  
+  // 奇偶判断
+  while(input != 1){
+    if (input % 2 == 0){
+      input = input /2;
+    }else if(input % 2 == 1){
+      input = (3 * input + 1) / 2;
+    }
+    steps = steps + 1;
+  }
+  
+  //输出步数
+  printf("%d", steps);
+  return 0;
+}
